d7fb6f8e5ef3e9acd8d1a91f6b960eafb8d19a55
fireph <443370+fireph@users.noreply.github.com>
2022-02-15 21:03:38 -0800

    format vite config and add validate to sentry source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,42 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
-import viteSentry from 'vite-plugin-sentry';
-import solidPlugin from 'vite-plugin-solid';
+import viteSentry from "vite-plugin-sentry";
+import solidPlugin from "vite-plugin-solid";
 
-const sentryEnv = process.env.SENTRY_ENV || ""
-const sentryToken = process.env.SENTRY_TOKEN || ""
+const sentryEnv = process.env.SENTRY_ENV || "";
+const sentryToken = process.env.SENTRY_TOKEN || "";
 
 export default defineConfig({
-  plugins: [solidPlugin(), viteSentry({
-    authToken: sentryToken,
-    org: "quordle",
-    project: "quordle",
-    release: process.env.npm_package_version,
-    deploy: {
-      env: sentryEnv
-    },
-    setCommits: {
-      auto: true
-    },
-    sourceMaps: {
-      include: ["./dist/assets"],
-      ignore: ["node_modules"],
-      urlPrefix: "~/assets"
-    },
-    debug: true
-  }), ViteEjsPlugin({
-    sentry_env: sentryEnv,
-    sentry_release: process.env.npm_package_version
-  })],
+  plugins: [
+    solidPlugin(),
+    viteSentry({
+      authToken: sentryToken,
+      org: "quordle",
+      project: "quordle",
+      release: process.env.npm_package_version,
+      deploy: {
+        env: sentryEnv,
+      },
+      setCommits: {
+        auto: true,
+      },
+      sourceMaps: {
+        include: ["./dist/assets"],
+        ignore: ["node_modules"],
+        urlPrefix: "~/assets",
+        validate: true,
+      },
+    }),
+    ViteEjsPlugin({
+      sentry_env: sentryEnv,
+      sentry_release: process.env.npm_package_version,
+    }),
+  ],
   build: {
     target: "es2015",
-    sourcemap: true
+    sourcemap: true,
   },
   server: {
-    host: true
-  }
+    host: true,
+  },
 });
